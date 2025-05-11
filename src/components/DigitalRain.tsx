cfbc8de191fd75fb28ae92be078acd1a55bb88a4
pmaitland <pmaitland98@gmail.com>
2025-05-11 19:21:46 +0100

    add droplet interface, remove shadows

--- src/components/DigitalRain.tsx
+++ src/components/DigitalRain.tsx
@@ -1,19 +1,27 @@
 import { ReactElement, useRef, useEffect, useState } from 'react'
 import '../assets/css/DigitalRain.scss'
 
+interface Droplet {
+  y: number
+  characters: string[]
+}
+
+function createDroplet(length: number, charSize: number, chars: string): Droplet {
+  return {
+    y: Math.floor(Math.random() * (window.innerHeight + length * charSize)),
+    characters: Array.from({ length: length }, () => chars[Math.floor(Math.random() * chars.length)])
+  }
+}
+
 function DigitalRain(): ReactElement {
   const ref = useRef<HTMLCanvasElement>(null)
 
-  const letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  const numbers = '0123456789'
-  const kana = 'ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ'
-  const chars = letters + numbers + kana
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789ｦｧｨｩｪｫｬｭｮｯｰｱｲｳｴｵｶｷｸｹｺｻｼｽｾｿﾀﾁﾂﾃﾄﾅﾆﾇﾈﾉﾊﾋﾌﾍﾎﾏﾐﾑﾒﾓﾔﾕﾖﾗﾘﾙﾚﾛﾜﾝ'
   const charSize = 24
   const dropletLength = 15
   const dropletOpaqueLength = dropletLength / 3
   const [columnCount, setColumnCount] = useState<number>(Math.floor(window.innerWidth / charSize) + 1)
-  const [dropletYs, setDropletYs] = useState<number[]>(Array.from({ length: columnCount }, () => Math.floor(Math.random() * (window.innerHeight + dropletLength * charSize))))
-  const [dropletChars, setDropletChars] = useState<string[][]>(Array.from({ length: columnCount }, () => Array.from({ length: dropletLength }, () => chars[Math.floor(Math.random() * chars.length)])))
+  const [droplets, setDroplets] = useState<Droplet[]>(Array.from({ length: columnCount }, () => createDroplet(dropletLength, charSize, chars)))
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -27,8 +35,7 @@ function DigitalRain(): ReactElement {
           if (columnCount < currentColumnCount) {
             let index = columnCount;
             while (index < currentColumnCount) {
-              dropletYs[index] = Math.floor(Math.random() * (window.innerHeight + dropletLength * charSize))
-              dropletChars[index] = Array.from({ length: dropletLength }, () => chars[Math.floor(Math.random() * chars.length)])
+              droplets[index] = createDroplet(dropletLength, charSize, chars)
               index++;
             }
             setColumnCount(Math.floor(window.innerWidth / charSize) + 1)
@@ -39,43 +46,38 @@ function DigitalRain(): ReactElement {
           context.fillStyle = "rgba(11, 248, 114, 1)"
           context.font = `${charSize}px 'Good Old DOS', monospace`
 
-          const newDropletYs = [...dropletYs]
-          const newDropletChars = [...dropletChars]
-          for (let droplet = 0; droplet < dropletYs.length; droplet++) {
+          const updatedDroplets = [...droplets]
+          for (let droplet = 0; droplet < droplets.length; droplet++) {
             for (let char = 0; char < dropletLength; char++) {
-              context.shadowBlur = Math.max(0, 8 - char)
               if (char === 0) {
                 context.fillStyle = "rgba(235, 235, 235, 1)"
-                context.shadowColor = 'white'
               } else if (char > 0 && char < dropletOpaqueLength) {
                 context.fillStyle = "rgba(11, 248, 114, 1)"
-                context.shadowColor = 'green'
               } else {
                 const alpha = 1 / (char - dropletOpaqueLength) * 1.5
                 context.fillStyle = `rgba(11, 248, 114, ${alpha})`
               }
-              context.fillText(dropletChars[droplet][char], droplet * charSize, dropletYs[droplet] - charSize * char)
+              context.fillText(droplets[droplet].characters[char], droplet * charSize, droplets[droplet].y - charSize * char)
 
               if (Math.floor(Math.random() * 100) === 0) {
-                newDropletChars[droplet][char] = chars[Math.floor(Math.random() * chars.length)]
+                updatedDroplets[droplet].characters[char] = chars[Math.floor(Math.random() * chars.length)]
               }
             }
 
-            newDropletChars[droplet].unshift(...newDropletChars[droplet].splice(-1))
+            updatedDroplets[droplet].characters.unshift(...updatedDroplets[droplet].characters.splice(-1))
 
-            newDropletYs[droplet] += charSize
-            if (newDropletYs[droplet] > window.innerHeight + dropletLength * charSize) {
-              newDropletYs[droplet] = 0
+            updatedDroplets[droplet].y += charSize
+            if (updatedDroplets[droplet].y > window.innerHeight + dropletLength * charSize) {
+              updatedDroplets[droplet].y = 0
             }
           }
-          setDropletYs(newDropletYs)
-          setDropletChars(newDropletChars)
+          setDroplets(updatedDroplets)
         }
       }
     }, 80)
 
     return () => clearInterval(interval)
-  }, [chars, columnCount, dropletYs, dropletChars, dropletOpaqueLength])
+  }, [chars, columnCount, droplets, dropletOpaqueLength])
 
   return <canvas className={'canvas'} ref={ref} />
 }
